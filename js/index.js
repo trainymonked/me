f2592e790c179cbe2ad694be68e97f0c2fca984e
trainymonked <47102288+trainymonked@users.noreply.github.com>
2022-11-19 21:14:05 +0300

    Nov 22

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-window.location.replace("https://t.me/trainymonked");
+const container = document.getElementsByClassName('container')[0];
+const label = document.getElementById('label');
+const suggestions = document.getElementById('suggestions');
+
+label.innerText = 'Wait 10 seconds to get redirected to my Telegram';
+
+const timer = setTimeout(() => window.location.replace("https://t.me/trainymonked"), 10000);
+const cancel = document.createElement('input');
+cancel.type = 'button';
+cancel.value = 'Cancel';
+cancel.onclick = () => {
+    clearTimeout(timer);
+    label.remove();
+    cancel.remove();
+};
+
+container.insertBefore(cancel, suggestions);
